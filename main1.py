ee0ee77aa73405353772a0f8255244c21b298dc5
zinny12 <125931489+zinny12@users.noreply.github.com>
2023-05-18 20:03:17 +0900

    Update main1.py

--- main1.py
+++ main1.py
@@ -5,16 +5,16 @@ import sys
 import telegram
 
 num = 0
-summary = ""
 
 chat_token = "5638730978:AAErxfMUsSu37fKHFHWMmpmbuig94t1qWQo"
 Jinny_id = "5711468830"
-  
-bot = telegram.Bot(token = chat_token) 
+token = "5638730978:AAErxfMUsSu37fKHFHWMmpmbuig94t1qWQo"  
+bot = telegram.Bot(chat_token) 
 
 Materials = ['SK머티리얼즈', '한솔케미칼', '솔브레인', '동진쎄미켐', '이엔에프테크놀로지', '덕산테코피아', '백광산업', '경인양행', '디엔에프', '오션브릿지', '와이씨켐', '지오엘리먼트', '엘티씨', '코미코', '후성', '티이엠씨', '레이크머티리얼즈', '원익머티리얼즈', '제이아이테크']
-
+summary = "[Materials]" + "\n"
 for company in Materials:
+summary = "▶" + company + "\n" 
     for n in range(1,41,10) : 
         response = requests.get('https://search.naver.com/search.naver?where=news&sm=tab_opt&sort=0&pd=4&ds=&query='+company+'&start='+str(n))
         soup = bs(response.text, 'html.parser')
@@ -23,10 +23,13 @@ for company in Materials:
             num = num + 1
             title = i.select_one('div.news_area > a').text.strip()
             link = i.select_one('a.news_tit').get('href')
-            text1 = company + ": " + title +"\n" + link
-            summary = summary + text1 + "\n"
-response = requests.post('https://api.telegram.org/bot5638730978:AAErxfMUsSu37fKHFHWMmpmbuig94t1qWQo/sendmessage?chat_id=5711468830&text='+summary)
-        
+            #text1 = title + "\n" + link + "\n" + "\n"
+            text1 = '\"<a href=\''+ link + '\'>'+ title + '</a>\"' "\n"
+            summary = summary + text1            
+#response = requests.post('https://api.telegram.org/bot5638730978:AAErxfMUsSu37fKHFHWMmpmbuig94t1qWQo/sendmessage?chat_id=5711468830&text='+summary)
+bot.send_message(chat_id = "5711468830", text = summary, parse_mode = ParseMode.HTML)
+#bot.send_message(chat_id = update.message.chat_id, text = "<a href='https://www.google.com/'>Google</a>", parse_mode = ParseMode.HTML)
+
 '''
 num = 0
 for n in range(1,41,10) : 
