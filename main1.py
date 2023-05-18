ac742448a4fab2b1c56f86733f5048601afe36de
zinny12 <125931489+zinny12@users.noreply.github.com>
2023-05-18 19:13:39 +0900

    Update main1.py

--- main1.py
+++ main1.py
@@ -14,20 +14,18 @@ bot = telegram.Bot(token = chat_token)
 
 Materials = ['SK머티리얼즈', '한솔케미칼', '솔브레인', '동진쎄미켐', '이엔에프테크놀로지', '덕산테코피아', '백광산업', '경인양행', '디엔에프', '오션브릿지', '와이씨켐', '지오엘리먼트', '엘티씨', '코미코', '후성', '티이엠씨', '레이크머티리얼즈', '원익머티리얼즈', '제이아이테크']
 
-
-for n in range(1,41,10) : 
-    response = requests.get('https://search.naver.com/search.naver?where=news&sm=tab_opt&sort=1&pd=7&ds=&query='+"하이닉스"+'&start='+str(n))
-    soup = bs(response.text, 'html.parser')
-    container = soup.select('ul.list_news > li')
-    for i in container:
-        num = num + 1
-        press = i.select_one('a.info.press').text.strip()
-        title = i.select_one('div.news_area > a').text.strip()
-        date = i.select_one('span.info').text.strip()
-        link = i.select_one('a.news_tit').get('href')
-        text = title +"\n" + link
-        #bot.sendMessage(chat_id = Jinny_id, text=text)
-        response = requests.post('https://api.telegram.org/bot5638730978:AAErxfMUsSu37fKHFHWMmpmbuig94t1qWQo/sendmessage?chat_id=5711468830&text='+text)
+for company in Materials:
+    for n in range(1,41,10) : 
+        response = requests.get('https://search.naver.com/search.naver?where=news&sm=tab_opt&sort=0&pd=4&ds=&query='+company+'&start='+str(n))
+        soup = bs(response.text, 'html.parser')
+        container = soup.select('ul.list_news > li')
+        for i in container:
+            num = num + 1
+            title = i.select_one('div.news_area > a').text.strip()
+            link = i.select_one('a.news_tit').get('href')
+            text1 = company + ": " + title +"\n" + link
+            summary = summary + text1 + "\n"
+response = requests.post('https://api.telegram.org/bot5638730978:AAErxfMUsSu37fKHFHWMmpmbuig94t1qWQo/sendmessage?chat_id=5711468830&text='+summary)
         
 '''
 num = 0
