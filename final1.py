2dc663086e60dc4e7d35ab0ac6bec7f22f1ba4ce
zinny12 <125931489+zinny12@users.noreply.github.com>
2023-05-22 10:49:19 +0900

    Update final1.py

--- final1.py
+++ final1.py
@@ -42,7 +42,7 @@ materials_value_chain = value_chain
 #####################################
 
 
-Parts = ['티씨케이', '하나머티리얼즈', '윌덱스', '비씨엔씨', '케이엔제이', '에스앤에스텍', '에프에스티', '아스플로', '한솔아이원스', '뉴파워프라즈마', '원익QnC', '메카로', '미코']
+Parts = ['티씨케이', '하나머티리얼즈', '윌덱스', '비씨엔씨', '케이엔제이', '에스앤에스텍', '펠리클', '에프에스티', '아스플로', '한솔아이원스', '뉴파워프라즈마', '원익QnC', '메카로', '미코']
 
 value_chain = "[Parts]" + "\n"
 
@@ -98,7 +98,7 @@ for company in Equipments:
 
 equipments_value_chain = value_chain
 
-OSAT = ['SFA반도체', '하나마이크론', '네패스', '엘비세미콘', '한양디지텍', '아이텍', '시그네틱스', '윈팩', '두산테스나', '네패스아크', '에이팩트', '큐알티', '심텍', '아비코전자', '해성디에스', '엠케이전자', '덕산하이메탈']
+OSAT = ['SFA반도체', '하나마이크론', '네패스', '엘비세미콘', '한양디지텍', '아이텍', '시그네틱스', '윈팩', '두산테스나', '네패스아크', '에이팩트', '큐알티']
 
 value_chain = "[OSAT]" + "\n"
 
@@ -125,6 +125,34 @@ for company in OSAT:
         pass
     
 OSAT_value_chain = value_chain
+
+PackagingMaterials = ['심텍', '아비코전자', 'DDR5', '해성디에스', '엠케이전자', '덕산하이메탈']
+
+value_chain = "[Packaging Materials]" + "\n"
+
+for company in PackagingMaterials:
+    summary = ""
+    for n in range(1,41,10) : 
+        response = requests.get('https://search.naver.com/search.naver?where=news&sm=tab_opt&sort=0&pd=4&ds=&query='+company+'&start='+str(n))
+        soup = bs(response.text, 'html.parser')
+        container = soup.select('ul.list_news > li')
+        for i in container:
+            title = i.select_one('div.news_area > a').text.strip()
+            if company in title:
+                link = i.select_one('a.news_tit').get('href')
+                title = title.replace('[', '')
+                title = title.replace(']', ' - ')
+                text1 = '[' + title + '](' + link + ')' + "\n" + "\n"
+                summary = summary + text1
+            else:
+                pass
+    companyname = "▶" + company + "\n" 
+    if summary !="":
+        value_chain = value_chain + companyname + summary
+    else:
+        pass    
+    
+Packaging_Materials_value_chain = value_chain
 
 
 TestParts = ['리노공업', 'ISC', '오킨스전자', '티에스이', '샘씨엔에스', '타이거일렉', '한미반도체', '이오테크닉스', '프로텍', '유니테스트', '피에스케이홀딩스', '코세스', '네오셈', '디아이', '엑시콘', '테크윙', '인텍플러스', '신성이엔지', '한양이엔지', '원방테크', '에스엠코어']
@@ -155,7 +183,35 @@ for company in TestParts:
 
 test_parts_value_chain = value_chain
 
-combined = materials_value_chain + "\n" + equipments_value_chain + "\n" +  parts_value_chain + "\n" + OSAT_value_chain + "\n" + test_parts_value_chain
+Distribution = ['미래반도체', '서플러스글로벌']
+
+value_chain = "[Distribution]" + "\n"
+
+for company in Distribution:
+    summary = ""
+    for n in range(1,41,10) : 
+        response = requests.get('https://search.naver.com/search.naver?where=news&sm=tab_opt&sort=0&pd=4&ds=&query='+company+'&start='+str(n))
+        soup = bs(response.text, 'html.parser')
+        container = soup.select('ul.list_news > li')
+        for i in container:
+            title = i.select_one('div.news_area > a').text.strip()
+            if company in title:
+                link = i.select_one('a.news_tit').get('href')
+                title = title.replace('[', '')
+                title = title.replace(']', ' - ')
+                text1 = '[' + title + '](' + link + ')' + "\n" + "\n"
+                summary = summary + text1
+            else:
+                pass
+    companyname = "▶" + company + "\n" 
+    if summary !="":
+        value_chain = value_chain + companyname + summary
+    else:
+        pass
+
+Distribution_value_chain = value_chain
+
+combined = materials_value_chain + "\n" + equipments_value_chain + "\n" +  parts_value_chain + "\n" + OSAT_value_chain + "\n" + Packaging_Materials_value_chain + "\n" + test_parts_value_chain + "\n" + Distribution_value_chain
 
 async def sendtelegram(message):
     await bot.send_message(chat_id = "5711468830", text = message, disable_web_page_preview= True, parse_mode = 'Markdown')
